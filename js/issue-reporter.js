711273bc23c130904e376af290da0d455736fe93
Manvel Saroyan <saroyanm@gmail.com>
2018-12-21 14:03:14 +0100

    1

--- js/issue-reporter.js
+++ js/issue-reporter.js
@@ -1,3 +1,20 @@
+function getBrowser() {
+  if (typeof chrome !== "undefined") {
+    if (typeof browser !== "undefined") {
+      return "Firefox";
+    } else {
+      return "Chrome";
+    }
+  } else {
+    return "Edge";
+  }
+}
+
+if (getBrowser() == "Chrome")
+{
+  window.browser = window.chrome;
+}
+
 function getPromise()
 {
   return browser.tabs.create({'active': false, 'url': 'http://google.com'});
