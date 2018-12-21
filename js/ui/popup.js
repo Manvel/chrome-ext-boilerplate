1780e663d62f311636f10fac7f26570da1d49c8d
Manvel Saroyan <saroyanm@gmail.com>
2018-12-21 13:39:58 +0100

    Added naviagtion to issue reporter

---
+++ js/ui/popup.js
@@ -0,0 +1,15 @@
+function reportIssue(tab)
+{
+  browser.tabs.create({
+    active: false,
+    url: browser.runtime.getURL("/issue-reporter.html?" + tab.id)
+  }).then(
+    // force closing popup which is not happening in Firefox
+    // @link https://issues.adblockplus.org/ticket/7017
+    () => window.close()
+  );
+}
+
+$("#issue-reporter").addEventListener(
+  "click", () => reportIssue(tab)
+);
